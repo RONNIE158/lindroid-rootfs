20b0ff5d55ff986f10b76a006be57042e1e8c422
RONNIE158 <ronnie158@live.com>
2025-04-08 19:23:14 +0700

    Update generate_recipe.py

--- generate_recipe.py
+++ generate_recipe.py
@@ -8,7 +8,7 @@ import argparse
 
 # List of architectures and UIs
 architectures = ["arm64"]
-uis = ["plasma"]
+uis = ["base"]
 
 # Common suffix for all files
 suffix = datetime.today().strftime('%Y%m%d')

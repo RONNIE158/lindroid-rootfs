b9a853dfcad7f3513e22eb5030d687553a3c0a48
RONNIE158 <ronnie158@live.com>
2025-04-21 01:13:53 +0700

    Update genimage.sh

--- rootfs-templates/scripts/genimage.sh
+++ rootfs-templates/scripts/genimage.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
-
 set -e
 
-ROOTFS_PATH=$(find ${PWD} -maxdepth 1 -mindepth 1 -type d -name '.debos-*' -printf '%T@ %p\n' | sort -nr | head -n 1 | awk '{ print $2 }')/root
-ROOTFS_SIZE=$(du -sm $ROOTFS_PATH | awk '{ print $1 }')
+ROOTFS_PATH=$(find "${PWD}" -maxdepth 1 -mindepth 1 -type d -name '.debos-*' -printf '%T@ %p\n' | sort -nr | head -n 1 | awk '{ print $2 }')/root
 
-ZIP_NAME=${1}
-WORK_DIR=${ZIP_NAME}.work
-OUT_PATH="out/${ZIP_NAME}.tar.gz"
-
-clean() {
-	rm -rf ${WORK_DIR}
-}
-trap clean EXIT
-
-# Create temporary work dir
-mkdir -p ${WORK_DIR}
 mkdir -p out
 
-# Create archive from rootfs
-tar -czvf "${OUT_PATH}" -C "${ROOTFS_PATH}" .
+echo "Packing rootfs to tar.gz..."
+tar -czf out/lindroid-kde-rootfs.tar.gz -C "${ROOTFS_PATH}" .
 
-# Copy output to current directory with fixed name for workflow (optional)
-cp "${OUT_PATH}" "./lindroid-kde-rootfs.tar.gz"
-
-echo "Generated archive:"
-ls -lh "${OUT_PATH}"
+echo "Done. Output:"
+ls -lh out/lindroid-kde-rootfs.tar.gz
